b05adbb1e42ba92d35647d3360c77b558c37ed04
UjjwalNexus <14062010ujjwal@gmail.com>
2024-10-12 16:18:18 +0530

    added a sum

--- Mains.py
+++ Mains.py
@@ -2,6 +2,18 @@ def sum (a,b):
     sum = a+b
     return sum
 
+
+# First sum
+
+
+a= int(input("Enter your first number: "))
+b= int(input("Enter your second number: "))
+result = sum(a,b)
+print ("The sum of ",a,"and ", b, "is : ", result)
+
+# Second Sum
+
+
 a= int(input("Enter your first number: "))
 b= int(input("Enter your second number: "))
 result = sum(a,b)
