7ff04562eecda7832824bfc505ba3913e810bb8e
UjjwalNexus <14062010ujjwal@gmail.com>
2024-10-13 11:39:31 +0530

    fibonacci

--- Mains.py
+++ Mains.py
@@ -1,4 +1,4 @@
-def sum (a,b):
+'''def sum (a,b):
     sum = a+b
     return sum
 
@@ -17,4 +17,21 @@ print ("The sum of ",a,"and ", b, "is : ", result)
 a= int(input("Enter your first number: "))
 b= int(input("Enter your second number: "))
 result = sum(a,b)
-print ("The sum of ",a,"and ", b, "is : ", result)
+print ("The sum of ",a,"and ", b, "is : ", result)'''
+
+
+def fibonacci():
+    first = 0
+    second = 1
+    third = 0
+    print ( first , end= "," )
+    i = 1
+   # while (i<=20):
+    for i in range (100):
+        third = second + first
+        print ( second  , end= ",")
+        first = second 
+        second = third
+        i+=1
+
+fibonacci()
